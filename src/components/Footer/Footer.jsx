2f9212bd100c88c0c328aa7c90bedcc070f05fd7
Weiyu Tang <twyla9876@outlook.com>
2022-10-13 20:18:10 +0100

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import SectionHeader from '../SectionHeader/SectionHeader'
 import './style.scss'
 
 const Footer = () => {
   return (
-    <section className={'footer-section'}>Footer</section>
+    <section className='footer-section'>
+      <SectionHeader title='Made in' />
+      <h1 className='location' id='location-text'>
+        Rio de Janeiro
+      </h1>
+    </section>
   )
 }
 
