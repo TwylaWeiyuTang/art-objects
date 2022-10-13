3246d563fc4c72df6f402d62ce3c4c3e1920b5f6
Weiyu Tang <twyla9876@outlook.com>
2022-10-13 19:22:16 +0100

    added about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './style.scss'
+import SectionHeader from '../SectionHeader/SectionHeader'
 
 const About = () => {
   return (
-    <section className={"about-section"}>index</section>
+    <section className="about-section">
+      <SectionHeader title='about'/>
+      <p id='headline'>
+        Flirty Flowers is a blog about flowers and the floral designers who make
+        them into art. Creativity and the art of ‘making’ require dialogue. The
+        full purpose of the Flirty Flowers blog is to encourage and inspire. We
+        value art, we value insight, and we value opinion.
+      </p>
+    </section>
   )
 }
 
