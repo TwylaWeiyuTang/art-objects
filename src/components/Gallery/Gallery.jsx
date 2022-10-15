20f021f266a8d2d3e6b3635cc89d9ccd01e8c3a7
Weiyu Tang <twyla9876@outlook.com>
2022-10-15 23:18:07 +0100

    added horizontal scroll with gsap for gallery part

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import classNames from 'classnames';
 import './style.scss'
+import { useEffect } from 'react';
+import { refresh } from 'less';
+import { useRef } from 'react';
+import useOnScreen from '../../hooks/useOnScreen'
 
 const images = [
   {
@@ -33,7 +40,15 @@ const images = [
 ];
 
 const GalleryItem = ({src, category, subtitle, title, updateActiveImage, index}) => {
-  return <div className='gallery-item-wrapper'>
+  const ref = useRef(null)
+  const onScreen = useOnScreen(ref, 0.5)
+
+  useEffect(() => {
+    if (onScreen) {
+      updateActiveImage(index)
+    }
+  }, [onScreen, index])
+  return <div className={classNames('gallery-item-wrapper', {'is-reveal': onScreen})} ref={ref}>
     <div />
     <div className='gallery-item'>
       <div className='gallery-item-info'>
@@ -51,15 +66,41 @@ const GalleryItem = ({src, category, subtitle, title, updateActiveImage, index})
   </div>
 }
 
-const Gallery = () => {
+const Gallery = ({src, index, columnOffset}) => {
+  const ref = useRef(null)
   const [activeImage, setActiveImage] = useState(1)
+
+  useEffect(() => {
+    setTimeout(() => {
+      let sections = gsap.utils.toArray('.gallery-item-wrapper')
+      console.log(sections)
+      gsap.to(sections, {
+        xPercent: -100 * (sections.length - 1),
+        ease: 'none',
+        scrollTrigger: {
+          start: 'top top',
+          trigger: ref.current,
+          scroller: '#main-container', // main scroller
+          pin: true,
+          scrub: 0.5,
+          snap: 1/(sections.length - 1),
+          end: () => `+=${ref.current.offsetWidth}`
+        }
+      })
+      ScrollTrigger.refresh()
+    })
+  }, [])
+
+  const handleUpdateActiveImage = (index) => {
+    setActiveImage(index + 1)
+  }
   return (
     <section className='gallery-section' data-scroll-section>
-      <div className='gallery'>
+      <div className='gallery' ref={ref}>
         <div className='gallery-counter'>
           <span>{activeImage}</span>
           <span className='divider' />
-          <span></span>
+          <span>{images.length}</span>
         </div>
         {images.map((image, index) => (
           <GalleryItem 
